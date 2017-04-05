25d7d6afe180ca959f227739103389729b073526
admin <admin>
2017-04-05 10:16:33 +0300

    Start file fibonacchi. Task 2_3

---
+++ src/com/tasks3/fibonacci/Fibonacci.java
@@ -0,0 +1,10 @@
+package com.tasks3.fibonacci;
+
+public class Fibonacci
+{
+    //Повертає число Фібоначчі за номером, нумерація почнеться з одиниці
+    //якщо число не можливо вирахувати поверніть -1
+    public long getNumber(int position){
+        return 1;
+    }
+}
