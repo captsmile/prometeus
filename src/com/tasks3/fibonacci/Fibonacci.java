281e34d8a57d0e8178e881a94f9ffa92388b9774
admin <admin>
2017-04-05 10:29:43 +0300

    final file fibonacchi. Task 2_3

--- src/com/tasks3/fibonacci/Fibonacci.java
+++ src/com/tasks3/fibonacci/Fibonacci.java
@@ -5,6 +5,11 @@ public class Fibonacci
     //Повертає число Фібоначчі за номером, нумерація почнеться з одиниці
     //якщо число не можливо вирахувати поверніть -1
     public long getNumber(int position){
-        return 1;
+        if (position <= 0) return -1;
+        if (position == 1) return 1;
+        if (position == 2) return 1;
+
+        return getNumber(position-1)+getNumber(position-2);
     }
+
 }
